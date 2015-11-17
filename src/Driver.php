7aae3005562bad462c3462493710b54eef0eafd1
Fabrice GUERY <fabrice@workdigital.co.uk>
2015-11-17 11:34:41 +0000

    Allow casperjs cmd to be passed to the Driver and throw if not available

--- src/Driver.php
+++ src/Driver.php
@@ -25,8 +25,14 @@ class Driver
 
     protected $jsInteraction;
 
-    public function __construct()
+    public function __construct($casperJsCommandPath = 'casperjs')
     {
+        if (!$this->isCommandExecutable($casperJsCommandPath)) {
+            throw new \Exception(
+                'Unable to execute ' . $casperJsCommandPath . '. '
+                . 'Ensure file exists in $PATH and exec() function is available.'
+            );
+        }
         $this->optionBuilder = new OptionsCliBuilder();
         $this->script .= "
 var casper = require('casper').create({
@@ -192,5 +198,23 @@ casper.page.customHeaders = {
     public function getScript()
     {
         return $this->script;
+    }
+
+    protected function isCommandExecutable($command)
+    {
+        if (!function_exists('exec')) {
+            return false;
+        }
+        if (substr(strtolower(PHP_OS), 0, 3) == 'win') {
+            // TODO find a way to test that, meanwhile just ignore.
+        } else {
+            exec('which ' . escapeshellarg($command), $output);
+            if (!$output) {
+                return false;
+            }
+        }
+
+
+        return true;
     }
 }
